ab737b092f7ea6f32b386165247ce37f05f9d293
JexonMG <143239658+JexonMG@users.noreply.github.com>
2023-09-01 01:24:25 -0600

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+def main ():
+    numero =int(input("Cual numero: "))
+    print("El cuadrado de numero es: ", cuadrado(numero))
+
+def suma(a, b):
+    return a + b
+
+def cuadrado(n):
+    return n + n
+
+if __name__ == '__main__':
+    main()  
